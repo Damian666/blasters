0436cffc2789d008a9e3dbf5dfe832cfcabcb16e
Robert Lodico <pandacoder@gdformula.net>
2013-06-03 22:41:22 -0400

    Added start of the Tileset class

---
+++ BlastersOnline/PuzzleGam/Levels/Tileset.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using TiledSharp;
+
+namespace BlastersGame.Levels
+{
+    /// <summary>
+    /// A Tileset is a model of the data contained within a specific tileset.
+    /// Wraps around <see cref="TmxTileset"/> and other things to abstract away tasks.
+    /// </summary>
+    public class Tileset
+    {
+        private TmxTileset _tileset;
+        private Texture2D _texture;
+
+        public Tileset(TmxTileset tileset)
+        {
+            _tileset = tileset;
+
+        }
+
+        public int GetTileTextureX(int gid)
+        {
+            long relativeGid = gid - (_tileset.FirstGid + 1);
+            int tiles = _tileset.Image.Width / _tileset.TileWidth;
+
+            return (int)(relativeGid % tiles);
+        }
+
+        public int GetTileTextureY(int gid)
+        {
+            long relativeGid = gid - (_tileset.FirstGid + 1);
+            int tiles = _tileset.Image.Width / _tileset.TileWidth;
+
+            return (int)(relativeGid / tiles);
+        }
+    }
+}
