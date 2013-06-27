47cf4a4d31e0f1ac093b2419ddad9927f8fcc2ca
Vaughan Hilts <hilts.vaughan@gmail.com>
2013-06-27 01:12:12 -0400

    Added map player spawning on inital game startup

--- BlastersOnline/AppServer/Services/Simulation/SimulationStateFactory.cs
+++ BlastersOnline/AppServer/Services/Simulation/SimulationStateFactory.cs
@@ -4,6 +4,7 @@ using BlastersShared.Game;
 using BlastersShared.Game.Entities;
 using BlastersShared.GameSession;
 using Microsoft.Xna.Framework;
+using TiledSharp;
 
 namespace AppServer.Services.Simulation
 {
@@ -41,16 +42,54 @@ namespace AppServer.Services.Simulation
             return simulationState;
         }
 
+        private static Stack<Vector2> GetPlayerSpawnPositions()
+        {
+            var currentMap = new TmxMap(string.Format(@"Content\Levels\{0}.tmx", "Battle_Royale"));
+            var pos = new List<Vector2>();
+
+            foreach (var obj in ((TmxObjectGroup) currentMap.ObjectGroups[0]).Objects )
+            {
+                var mapObject = (TmxObjectGroup.TmxObject) obj;
+
+                if (mapObject.Type == "PlayerSpawn")
+                {
+                    Vector2 vector = new Vector2(mapObject.X - 16, mapObject.Y - 6);
+                    pos.Add(vector);
+                }
+
+            }
+
+            pos.Shuffle();
+            return new Stack<Vector2>(pos);
+
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+
         private static SimulationState CreateDeathmatchState(GameSession gameSession)
         {
 
             // An empty simulation state; we'll begin filling it up
             var simulationState = new SimulationState();
+            var positionStack = GetPlayerSpawnPositions();
 
             foreach (var user in gameSession.Users)
             {
                 // Create a player from each user and shove them down the container
-                var player = EntityFactory.CreatePlayer(user, new Vector2(116, 86));
+                var player = EntityFactory.CreatePlayer(user, positionStack.Pop() );
                 simulationState.Entities.Add(player);
             }
 
