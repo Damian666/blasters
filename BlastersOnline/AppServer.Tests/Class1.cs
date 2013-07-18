0558c50b1fd3ae954411b2c01da2c0038aaec027
Vaughan Hilts <hilts.vaughan@gmail.com>
2013-07-18 00:44:07 -0400

    m0ar

---
+++ BlastersOnline/AppServer.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace AppServer.Tests
+{
+   public class TestFixture
+   {
+       [Fact]
+       public void MyTest()
+       {
+           Assert.Equal(4, 2 + 2);
+       }
+
+
+
+   }
+
+
+
+}
---
+++ BlastersOnline/AppServer.Tests/LobbyServer/Services/AuthentationServiceTests.cs
@@ -0,0 +1,17 @@
+﻿
+using Xunit;
+
+namespace AppServer.Tests.LobbyServer.Services
+{
+    public class AuthentationServiceTests
+    {
+
+        [Fact]
+        public void AddAndVerifyUser()
+        {
+            Assert.Equal(0, 0);
+        }
+
+
+    }
+}
