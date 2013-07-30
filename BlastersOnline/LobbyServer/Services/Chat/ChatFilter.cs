ac1749b1e88ff8adbf3101b89807a233147ad97e
Vaughan Hilts <hilts.vaughan@gmail.com>
2013-07-30 19:39:44 -0400

    Created quest service stub

---
+++ BlastersOnline/LobbyServer/Services/Chat/ChatFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobbyServer.Services.Chat
+{
+    /// <summary>
+    /// The chat filter is an object that procceses strings and delegates where they will be sent and censored before dispatch.
+    /// </summary>
+    public class ChatFilter
+    {
+    }
+}
---
+++ BlastersOnline/LobbyServer/Services/QuestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlastersShared.Services;
+
+namespace LobbyServer.Services
+{
+    public class QuestService : Service
+    {
+        public override void PeformUpdate()
+        {
+            //TODO: Implement the quest service instead of stubbing it out
+            throw new NotImplementedException();
+        }
+    }
+}
