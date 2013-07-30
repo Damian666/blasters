e834ba6052c6a8fe91a72de093868f6511b9fe5c
Vaughan Hilts <hilts.vaughan@gmail.com>
2013-07-29 21:57:21 -0400

    And more

---
+++ BlastersOnline/BlastersShared/Models/Enum/UserIntents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlastersShared.Models.Enum
+{
+    /// <summary>
+    /// The user intents enum
+    /// </summary>
+    [Flags]
+    public enum UserIntents : long 
+    {   
+        // This intent value indicates this user is interested in session updates
+        SessionUpdates = 0x0,
+
+        // This intent value indicates this user is interested in updates about friends signing in and out
+        FriendsUpdates = 0x1
+    }
+}
---
+++ BlastersOnline/LobbyServer/Services/UserIntentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LobbyServer.Services
+{
+    /// <summary>
+    /// The user intent service is used for managing intents that a particular client has expressed interest in.
+    /// </summary>
+    public class UserIntentService
+    {
+        //TODO: Implement the intent service
+    }
+}
